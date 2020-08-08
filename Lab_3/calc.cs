b075c915d3b392ba4ae8711dcb6543b22336876a
Naiya Prajapati <55459380+NaiyaPrajapati@users.noreply.github.com>
2020-08-09 00:00:36 +0530

    Add files via upload

---
+++ Lab_3/calc.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Class{
+
+		public class Class1{
+			public static int Add(int a,int b){
+					return a+b;
+			}
+		public static int Multiply(int a,int b){
+					return a*b;
+		}
+	}
+}
---
+++ Lab_3/calc_new.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Class{
+
+		public class Class1{
+			public static int Add(int a,int b){
+					return a+b;
+			}
+		public static int Multiply(int x,int y){
+					return x*y;
+		}
+	}
+}
